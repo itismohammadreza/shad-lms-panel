74674f44713cd6d59a57a305c9b99f8fe2165b6d
PC-22 <shahbaziweb@gmail.com>
2023-02-26 15:11:47 +0330

    change auth guard logic

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -9,28 +9,15 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService) {
   }
 
-  async canActivate(
+  canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Promise<boolean> {
-    const role = this.authService.me.role.toLowerCase();
-    const currentUrl = state.url.toLowerCase();
-    let targetUrl;
-
-    switch (role) {
-      case 'principal':
-      case 'teacher':
-        targetUrl = '/teacher'
-        break;
-      case 'student':
-        targetUrl = '/student'
-        break;
+  ): boolean {
+    if (this.authService.hasToken()) {
+      return true;
+    } else {
+      this.router.navigate(['/auth/login'], {queryParams: {returnUrl: state.url}});
+      return false;
     }
-
-    if (!currentUrl.includes(targetUrl)) {
-      await this.router.navigateByUrl(targetUrl)
-      return false
-    }
-    return true
   }
 }
