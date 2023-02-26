6a2f6536362e9b3c7a4d9f8c2dbaea7252b186cb
PC-22 <shahbaziweb@gmail.com>
2023-02-26 15:11:02 +0330

    remove token-param.guard.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import {NgModule, Type} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {TokenParamGuard} from "@core/guard";
 import {StatusComponent} from "@ng/components/status/status.component";
 
 export const routes: Routes = [
   {
     path: '',
-    canActivate: [TokenParamGuard],
     loadChildren: (): Promise<Type<any>> =>
       import('./modules/main/main.module').then((m) => m.MainModule),
   },
--- src/app/core/guard/index.ts
+++ src/app/core/guard/index.ts
@@ -1,3 +1,2 @@
 export * from './auth.guard';
 export * from './open-dialog.guard';
-export * from './token-param.guard';
