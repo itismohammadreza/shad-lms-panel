dc52e0e0ccb149722f665d6836d29bf4dc48956c
PC-22 <shahbaziweb@gmail.com>
2023-02-26 15:13:17 +0330

    remove extra components

--- src/app/core/http/index.ts
+++ src/app/core/http/index.ts
@@ -1,3 +1,3 @@
 export * from './api.service';
 export * from './auth.service';
-export * from './exam.service';
+export * from './data.service';
--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,22 +1,6 @@
 import {Type} from '@angular/core';
-import {
-  DescriptiveAnswerItemComponent
-} from "@shared/components/descriptive-answer-item/descriptive-answer-item.component";
-import {ExamFilePreviewComponent} from "@shared/components/exam-file-preview/exam-file-preview.component";
-import {MultiAnswerItemComponent} from "@shared/components/multi-answer-item/multi-answer-item.component";
-import {PageContainerComponent} from "@shared/components/page-container/page-container.component";
-import {ExamItemComponent} from "@shared/components/exam-item/exam-item.component";
-import {HistoryItemComponent} from "@shared/components/history-item/history-item.component";
-import {TutorialItemComponent} from "@shared/components/tutorial-item/tutorial-item.component";
-import {TutorialFilePreviewComponent} from "@shared/components/tutorial-file-preview/tutorial-file-preview.component";
+import {LogoComponent} from "@shared/components/logo/logo.component";
 
 export const COMPONENTS: Type<any>[] = [
-  DescriptiveAnswerItemComponent,
-  ExamFilePreviewComponent,
-  ExamItemComponent,
-  HistoryItemComponent,
-  MultiAnswerItemComponent,
-  PageContainerComponent,
-  TutorialItemComponent,
-  TutorialFilePreviewComponent
+  LogoComponent
 ];
