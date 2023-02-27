d1a0d003bbe9a814e3bdbfb5e56835d634f5d9a7
PC-22 <shahbaziweb@gmail.com>
2023-02-27 17:06:12 +0330

    remove extra services usage

--- src/app/ng/services/index.ts
+++ src/app/ng/services/index.ts
@@ -1,7 +1,4 @@
-export * from './breadcrumb.service';
 export * from './domhandler';
-export * from './dynamic-dialog.service';
 export * from './moment.service';
 export * from './utils.service';
-export * from './persian.service';
 export * from './overlay.service';
