88ab04f479a7c40cd5e9d139a51a56a762aded59
PC-22 <shahbaziweb@gmail.com>
2023-02-27 17:07:22 +0330

    implement pages routes

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -1,8 +1,10 @@
 import {RouterModule, Routes} from '@angular/router';
-import {NgModule, Type} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {MainPage} from './main.page';
-import {AuthGuard} from "@core/guard";
 import {DashboardPage} from "@modules/main/pages/dashboard/dashboard.page";
+import {ExamPage} from "@modules/main/pages/exam/exam.page";
+import {HomeworkPage} from "@modules/main/pages/homework/homework.page";
+import {TutorialPage} from "@modules/main/pages/tutorial/tutorial.page";
 
 const routes: Routes = [
   {
@@ -10,13 +12,28 @@ const routes: Routes = [
     component: MainPage,
     children: [
       {
-        path: '',
+        path: 'dashboard',
         component: DashboardPage,
         title: 'داشبورد'
       },
       {
+        path: 'exam',
+        component: ExamPage,
+        title: 'داشبورد'
+      },
+      {
+        path: 'homework',
+        component: HomeworkPage,
+        title: 'تکالیف'
+      },
+      {
+        path: 'tutorial',
+        component: TutorialPage,
+        title: 'درسنامه'
+      },
+      {
         path: '',
-        redirectTo: '',
+        redirectTo: 'dashboard',
         pathMatch: 'full'
       },
       {
