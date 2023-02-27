3fecda1338d40c106ec1960911702e8537026439
PC-22 <shahbaziweb@gmail.com>
2023-02-27 17:07:30 +0330

    create navbar-menu.component

---
+++ src/app/modules/main/components/navbar-menu/navbar-menu.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'ng-navbar-menu',
+  templateUrl: './navbar-menu.component.html',
+  styleUrls: ['./navbar-menu.component.scss']
+})
+export class NavbarMenuComponent implements OnInit {
+  items: MenuItem[] = [
+    {
+      label: 'داشبورد',
+      routerLink: '/dashboard',
+      routerLinkActiveOptions: 'p-menuitem-link-active'
+    },
+    {
+      label: 'آزمون',
+      routerLink: '/exam'
+    },
+    {
+      label: 'تکالیف',
+      routerLink: '/homework'
+    },
+    {
+      label: 'درسنامه',
+      routerLink: '/tutorial'
+    }
+  ];
+
+  ngOnInit() {
+  }
+}
