deef7430e8b37a7fc8d080d655e49eb9ed903756
PC-22 <shahbaziweb@gmail.com>
2023-02-27 17:08:17 +0330

    create tutorial.page

---
+++ src/app/modules/main/pages/tutorial/tutorial.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-tutorial',
+  templateUrl: './tutorial.page.html',
+  styleUrls: ['./tutorial.page.scss']
+})
+export class TutorialPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
