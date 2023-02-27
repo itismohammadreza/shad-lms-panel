3c8c93c1fe2e06fff42f0c5b85cf97beab335a8d
PC-22 <shahbaziweb@gmail.com>
2023-02-27 17:05:57 +0330

    remove extra pipes usage

--- src/app/ng/pipes/index.ts
+++ src/app/ng/pipes/index.ts
@@ -1,18 +1,8 @@
 import {Type} from '@angular/core';
-import {DatePipe} from './date.pipe';
-import {DotsPipe} from './dots.pipe';
-import {FileNamePipe} from './file-name.pipe';
 import {FilterPipe} from './filter.pipe';
 import {SafePipe} from './safe.pipe';
-import {ToBase64Pipe} from './to-base64.pipe';
-import {MonthTextPipe} from './month-text.pipe';
 
 export const PIPES: Type<any>[] = [
-  DotsPipe,
   SafePipe,
-  DatePipe,
   FilterPipe,
-  FileNamePipe,
-  ToBase64Pipe,
-  MonthTextPipe
 ];
