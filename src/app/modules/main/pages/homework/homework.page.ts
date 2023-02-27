4e04be83bddc790bf638ad2fb027ea34719c748d
PC-22 <shahbaziweb@gmail.com>
2023-02-27 17:05:16 +0330

    create homework.page

---
+++ src/app/modules/main/pages/homework/homework.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ng-homework',
+  templateUrl: './homework.page.html',
+  styleUrls: ['./homework.page.scss']
+})
+export class HomeworkPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
