16d91bc4f41768fe11a7d220bca1dd091919553c
PC-22 <shahbaziweb@gmail.com>
2023-03-04 17:35:39 +0330

    add profile-edit.page

---
+++ src/app/modules/main/pages/profile-edit/profile-edit.page.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'ng-profile-edit',
+  templateUrl: './profile-edit.page.html',
+  styleUrls: ['./profile-edit.page.scss']
+})
+export class ProfileEditPage implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSelectImage(event: any) {
+
+  }
+}
