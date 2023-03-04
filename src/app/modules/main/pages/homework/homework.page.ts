852208567174c75488b8f1a2c78150970e3dfb1a
PC-22 <shahbaziweb@gmail.com>
2023-03-04 17:37:26 +0330

    implement homework.page

--- src/app/modules/main/pages/homework/homework.page.ts
+++ src/app/modules/main/pages/homework/homework.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ng-homework',
@@ -7,7 +7,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeworkPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
+
+  stackedData = {
+    labels: ['تهران', 'اصفهان', 'شیراز', 'اردبیل', 'قم', 'تبریز', 'مشهد'],
+    datasets: [{
+      type: 'bar',
+      backgroundColor: '#42A5F5',
+      data: [
+        50,
+        25,
+        12,
+        48,
+        90,
+        76,
+        42
+      ]
+    }, {
+      type: 'bar',
+      backgroundColor: '#66BB6A',
+      data: [
+        21,
+        84,
+        24,
+        75,
+        37,
+        65,
+        34
+      ]
+    }, {
+      type: 'bar',
+      backgroundColor: '#FFA726',
+      data: [
+        41,
+        52,
+        24,
+        74,
+        23,
+        21,
+        32
+      ]
+    }]
+  };
+  stackedOptions = {
+    plugins: {
+      legend: {display: false},
+      tooltips: {
+        mode: 'index',
+        intersect: false
+      },
+      // legend: {
+      //   labels: {
+      //     color: '#495057'
+      //   }
+      // }
+    },
+    scales: {
+      x: {
+        stacked: false,
+        ticks: {
+          font: {family: 'IRANSans'},
+        },
+        // ticks: {
+        //   color: '#495057'
+        // },
+        // grid: {
+        //   color: '#ebedef'
+        // }
+      },
+      y: {
+        stacked: false,
+        ticks: {
+          font: {family: 'IRANSans'},
+        }
+        // ticks: {
+        //   color: '#495057'
+        // },
+        // grid: {
+        //   color: '#ebedef'
+        // }
+      }
+    }
+  };
 
   ngOnInit(): void {
   }
