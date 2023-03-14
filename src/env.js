447a202dafda9e729a286ec5ca1ee8851f0d79c8
PC-22 <shahbaziweb@gmail.com>
2023-03-14 10:28:21 +0330

    set base api url

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  const API_URL = 'https://shadlms.iranlms.ir';
+  const API_URL = 'https://shadlms.iranlms.ir/panel';
   window.__env.apiUrl = API_URL;
 }(this));
